257b706b48d2c82fa93eb61309bfaedc77ce843d
AlexiSkyline <ilegal_sprite@outlook.com>
2023-01-05 08:17:53 -0600

    Eureka Server added

--- car-service/src/main/java/org/alexiskyline/car/CarApplication.java
+++ car-service/src/main/java/org/alexiskyline/car/CarApplication.java
@@ -2,8 +2,10 @@ package org.alexiskyline.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarApplication.class, args);
